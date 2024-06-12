16a2471a0a5aa3ad4058db142ce7cd8d8f49566a
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2024-06-12 02:28:15 +0100

    fix/wss_connection

--- hivemind_bus_client/client.py
+++ hivemind_bus_client/client.py
@@ -114,8 +114,8 @@ class HiveMessageBusClient(OVOSBusClient):
 
         # NOTE: self._host and self._port accessed only after self.init_identity()
         # this allows them to come from set-identity cli command
-        host = self._host.replace("ws://", "").replace("wss://", "").strip()
         use_ssl = host.startswith("wss://")
+        host = self._host.replace("ws://", "").replace("wss://", "").strip()
         super().__init__(host=host, port=self._port, ssl=use_ssl,
                          emitter=EventEmitter(), session=sess)
 
