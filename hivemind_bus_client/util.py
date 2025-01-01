c00d11e520863f88ec317d0eeb8c7df5f485ccd2
miro <jarbasai@mailfence.com>
2025-01-01 22:06:20 +0000

    performance: reduce_payload_size
    change encoding of encrypted payloads to reduce message size

--- hivemind_bus_client/util.py
+++ hivemind_bus_client/util.py
@@ -1,8 +1,8 @@
 import json
 import zlib
-from binascii import hexlify
-from binascii import unhexlify
+from binascii import hexlify, unhexlify
 
+import pybase64
 from ovos_utils.security import encrypt, decrypt, AES
 
 from hivemind_bus_client.exceptions import EncryptionKeyError, DecryptionKeyError
@@ -101,13 +101,20 @@ def get_mycroft_msg(pload):
     return pload
 
 
-def encrypt_as_json(key, data):
+def encrypt_as_json(key, data, b64=False):
+    # TODO default b64 to True in a future release
+    #  we dont want clients to update before servers, otherwise servers won't be able to decode
+    #  after a reasonable time all servers should support decoding both schemes and the default can change
     if isinstance(data, dict):
         data = json.dumps(data)
     if len(key) > 16:
         key = key[0:16]
     ciphertext = encrypt_bin(key, data)
     nonce, ciphertext, tag = ciphertext[:16], ciphertext[16:-16], ciphertext[-16:]
+    if b64:
+        return json.dumps({"ciphertext": pybase64.b64encode(ciphertext).decode('utf-8'),
+                           "tag": pybase64.b64encode(tag).decode('utf-8'),
+                           "nonce": pybase64.b64encode(nonce).decode('utf-8')})
     return json.dumps({"ciphertext": hexlify(ciphertext).decode('utf-8'),
                        "tag": hexlify(tag).decode('utf-8'),
                        "nonce": hexlify(nonce).decode('utf-8')})
@@ -118,16 +125,34 @@ def decrypt_from_json(key, data):
         data = json.loads(data)
     if len(key) > 16:
         key = key[0:16]
-    ciphertext = unhexlify(data["ciphertext"])
-    if data.get("tag") is None:  # web crypto
-        ciphertext, tag = ciphertext[:-16], ciphertext[-16:]
-    else:
-        tag = unhexlify(data["tag"])
-    nonce = unhexlify(data["nonce"])
+
+    # payloads can be either hex encoded (old style)
+    # or b64 encoded (new style)
+    def decode(b64=False):
+        if b64:
+            decoder = pybase64.b64decode
+        else:
+            decoder = unhexlify
+
+        ciphertext = decoder(data["ciphertext"])
+        if data.get("tag") is None:  # web crypto
+            ciphertext, tag = ciphertext[:-16], ciphertext[-16:]
+        else:
+            tag = decoder(data["tag"])
+        nonce = decoder(data["nonce"])
+        return ciphertext, tag, nonce
+
+    is_b64 = any(a.isupper() for a in str(data))  # if any letter is uppercase, it must be b64
+    ciphertext, tag, nonce = decode(is_b64)
     try:
         return decrypt(key, ciphertext, tag, nonce)
-    except ValueError:
-        raise DecryptionKeyError
+    except ValueError as e:
+        if not is_b64:
+            try:  # maybe it was b64 after all? unlikely but....
+                ciphertext, tag, nonce = decode(b64=True)
+                return decrypt(key, ciphertext, tag, nonce)
+            except ValueError:
+                raise DecryptionKeyError from e
 
 
 def encrypt_bin(key, data):
@@ -169,7 +194,7 @@ def decompress_payload(compressed):
     # Decompressing text
     if isinstance(compressed, str):
         # assume hex
-        compressed = unhexlify(compressed)
+        compressed = pybase64.b64decode(compressed)
     return zlib.decompress(compressed)
 
 
@@ -189,3 +214,19 @@ def bytes2str(payload, compressed=False):
         return decompress_payload(payload).decode("utf-8")
     else:
         return payload.decode("utf-8")
+
+
+if __name__ == "__main__":
+    k = "*" * 16
+    test = "this is a test text for checking size of encryption and stuff"
+    print(len(test))  # 61
+
+    encjson = encrypt_as_json(k, test, b64=True)
+    # {"ciphertext": "MkTc1LSK3jugt5SXapAeSrD6YWnYdSJ5oqF2bWYcnFpAYgjAgcTFXiKL3wBsqVKY52SkO5mjkqr7i/0A5A==", "tag": "37WNN8e23Mj0LlOxu9cjnQ==", "nonce": "inRwcb0H1Xu6pp80WFeJvg=="}
+    print(len(encjson))  # 174
+    assert decrypt_from_json(k, encjson) == test
+
+    encjson = encrypt_as_json(k, test, b64=False)
+    # {"ciphertext": "64c65bad86a3582097aa4958b7c9555e8bf7deeac6bdf8b5f648cc360aaf50062ae9c635f602b3c66b2de1eece57666b3412a26f55bbd5ace2f601d8c2", "tag": "ce550c1e399c92bb26bf3c171c212e7d", "nonce": "84d045071b05bf005145ce071df0ed41"}
+    print(len(encjson))  # 228
+    assert decrypt_from_json(k, encjson) == test
